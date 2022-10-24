f5ed68bdea130b5d504e9014d5bc67008b49295d
mog <mog@pixiv.com>
2022-10-24 17:07:46 +0900

    🚂

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production'
+
 module.exports = {
   reactStrictMode: true,
+  assetPrefix: isProd ? 'https://mo-repros.github.io/charcoal-ui-border-default/' : ''
 }
