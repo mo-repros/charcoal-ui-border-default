90ccb270980f8ec721b8aea313f5328e31bb34ba
mog <mog@pixiv.com>
2022-10-24 16:49:42 +0900

    💤

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
+const { light, dark } = require('@charcoal-ui/theme')
+const { createTailwindConfig } = require('@charcoal-ui/tailwind-config')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  media: false,
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  presets: [
+    createTailwindConfig({
+      version: 'v3',
+      theme: {
+        ':root': light,
+        'html[data-theme="dark"]': dark
+      },
+    }),
+  ]
 }
